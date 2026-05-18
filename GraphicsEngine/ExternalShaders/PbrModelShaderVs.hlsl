d36e335b37bc0d7bee3b914d6f02b099cecbd9a4
Vilgot <122538342+VilgotBohlenius@users.noreply.github.com>
2026-05-18 18:08:18 +0200

    updated pbr shaders to match our input data

--- GraphicsEngine/ExternalShaders/PbrModelShaderVs.hlsl
+++ GraphicsEngine/ExternalShaders/PbrModelShaderVs.hlsl
@@ -1,33 +1,37 @@
-#include "Common.hlsli"
+#include "../Common.hlsli"
 
-ModelVertexToPixel main(ModelVertexInput input)
+PixelInputType main(VertexInputType input)
 {
-	ModelVertexToPixel result;
-
+    PixelInputType result;
+	
 	float4 vertexObjectPos = input.position;
-	float4 vertexWorldPos = mul(ObjectToWorld, vertexObjectPos);
-	float4 vertexViewPos = mul(WorldToCamera, vertexWorldPos);
-	float4 vertexProjectionPos = mul(CameraToProjection, vertexViewPos);
+    //float4 vertexWorldPos = mul(modelToWorldMatrix, vertexObjectPos);
+    //float4 vertexViewPos = mul(worldToClipMatrix, vertexWorldPos);
+	//float4 vertexProjectionPos = mul(CameraToProjection, vertexViewPos);
 
-	float3x3 toWorldRotation = (float3x3)ObjectToWorld;
+    float4 vertexWorldPos = mul(modelToWorldMatrix, vertexObjectPos);
+    float4 vertexProjectionPos = mul(worldToClipMatrix, vertexWorldPos);
+	
+    float3x3 toWorldRotation = (float3x3) modelToWorldMatrix;
 	float3 vertexWorldNormal = mul(toWorldRotation, input.normal);
-	float3 vertexWorldBinormal = mul(toWorldRotation, input.binormal);
+	float3 vertexWorldBinormal = mul(toWorldRotation, input.bitangent);
 	float3 vertexWorldTangent = mul(toWorldRotation, input.tangent);
 	
-	result.position = vertexProjectionPos;
+	result.screenPosition = vertexProjectionPos;
 	result.worldPosition = vertexWorldPos;
-	result.vertexColor0 = input.vertexColor0;
-	result.vertexColor1 = input.vertexColor1;
-	result.vertexColor2 = input.vertexColor2;
-	result.vertexColor3 = input.vertexColor3;
+	//result.vertexColor0 = input.vertexColor0;
+	//result.vertexColor1 = input.vertexColor1;
+	//result.vertexColor2 = input.vertexColor2;
+	//result.vertexColor3 = input.vertexColor3;
 
-	result.texCoord0 = input.texCoord0;
-	result.texCoord1 = input.texCoord1;
-	result.texCoord2 = input.texCoord2;
-	result.texCoord3 = input.texCoord3;
+	result.uv = input.uv;
+	//result.texCoord0 = input.texCoord0;
+	//result.texCoord1 = input.texCoord1;
+	//result.texCoord2 = input.texCoord2;
+	//result.texCoord3 = input.texCoord3;
 
 	result.normal = vertexWorldNormal;
-	result.binormal = vertexWorldBinormal;
+	result.bitangent = vertexWorldBinormal;
 	result.tangent = vertexWorldTangent;
 	
 	return result;
